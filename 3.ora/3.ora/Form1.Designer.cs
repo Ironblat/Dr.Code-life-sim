ca4b8971ab17a5c0d4d96c23a824abfbaa832675
Ironblyat <77847032+Ironblat@users.noreply.github.com>
2022-10-07 20:17:11 +0200

    Add files via upload
    Form1.cs a fő fájl.  A 3.ora mappában lesz. Visual Stúdióban készült.

---
+++ 3.ora/3.ora/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace _3.ora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nev_tb = new System.Windows.Forms.TextBox();
+            this.ok_btn = new System.Windows.Forms.Button();
+            this.visszajaro_lbl = new System.Windows.Forms.Label();
+            this.penzosszeg_tb = new System.Windows.Forms.TextBox();
+            this.menu_lbl = new System.Windows.Forms.Label();
+            this.item_lbl = new System.Windows.Forms.Label();
+            this.bal_lbl = new System.Windows.Forms.Label();
+            this.asd_lbl = new System.Windows.Forms.Label();
+            this.work_btn = new System.Windows.Forms.Button();
+            this.Time_PB = new System.Windows.Forms.ProgressBar();
+            this.workh_TB = new System.Windows.Forms.TrackBar();
+            this.trackbarvalue_lbl = new System.Windows.Forms.Label();
+            this.progressbar_lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.workh_TB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nev_tb
+            // 
+            this.nev_tb.Location = new System.Drawing.Point(78, 62);
+            this.nev_tb.Name = "nev_tb";
+            this.nev_tb.Size = new System.Drawing.Size(100, 23);
+            this.nev_tb.TabIndex = 0;
+            // 
+            // ok_btn
+            // 
+            this.ok_btn.Location = new System.Drawing.Point(78, 104);
+            this.ok_btn.Name = "ok_btn";
+            this.ok_btn.Size = new System.Drawing.Size(75, 23);
+            this.ok_btn.TabIndex = 1;
+            this.ok_btn.Text = "Ok";
+            this.ok_btn.UseVisualStyleBackColor = true;
+            this.ok_btn.Click += new System.EventHandler(this.ok_btn_Click);
+            // 
+            // visszajaro_lbl
+            // 
+            this.visszajaro_lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.visszajaro_lbl.Location = new System.Drawing.Point(310, 70);
+            this.visszajaro_lbl.Name = "visszajaro_lbl";
+            this.visszajaro_lbl.Size = new System.Drawing.Size(144, 15);
+            this.visszajaro_lbl.TabIndex = 2;
+            this.visszajaro_lbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // penzosszeg_tb
+            // 
+            this.penzosszeg_tb.Enabled = false;
+            this.penzosszeg_tb.Location = new System.Drawing.Point(78, 16);
+            this.penzosszeg_tb.Name = "penzosszeg_tb";
+            this.penzosszeg_tb.Size = new System.Drawing.Size(100, 23);
+            this.penzosszeg_tb.TabIndex = 3;
+            // 
+            // menu_lbl
+            // 
+            this.menu_lbl.AutoSize = true;
+            this.menu_lbl.Location = new System.Drawing.Point(48, 185);
+            this.menu_lbl.Name = "menu_lbl";
+            this.menu_lbl.Size = new System.Drawing.Size(156, 165);
+            this.menu_lbl.TabIndex = 4;
+            this.menu_lbl.Text = "Menü:\r\n\r\n-Pizza: 450 Ft\r\n-Hamburger: 750 Ft\r\n-Kóla: 650 Ft\r\n-Popcorn: 100 Ft\r\n-Sa" +
+    "láta: 1000 Ft\r\n-Villanyszámla: 100000000 Ft\r\n\r\n\r\n\r\n";
+            this.menu_lbl.Click += new System.EventHandler(this.menu_lbl_Click);
+            // 
+            // item_lbl
+            // 
+            this.item_lbl.AutoSize = true;
+            this.item_lbl.Location = new System.Drawing.Point(12, 65);
+            this.item_lbl.Name = "item_lbl";
+            this.item_lbl.Size = new System.Drawing.Size(57, 15);
+            this.item_lbl.TabIndex = 5;
+            this.item_lbl.Text = "Rendelés:";
+            this.item_lbl.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // bal_lbl
+            // 
+            this.bal_lbl.AutoSize = true;
+            this.bal_lbl.Location = new System.Drawing.Point(12, 19);
+            this.bal_lbl.Name = "bal_lbl";
+            this.bal_lbl.Size = new System.Drawing.Size(58, 15);
+            this.bal_lbl.TabIndex = 6;
+            this.bal_lbl.Text = "Egyenleg:";
+            // 
+            // asd_lbl
+            // 
+            this.asd_lbl.AutoSize = true;
+            this.asd_lbl.Location = new System.Drawing.Point(234, 69);
+            this.asd_lbl.Name = "asd_lbl";
+            this.asd_lbl.Size = new System.Drawing.Size(61, 15);
+            this.asd_lbl.TabIndex = 7;
+            this.asd_lbl.Text = "Visszajáró:";
+            // 
+            // work_btn
+            // 
+            this.work_btn.Location = new System.Drawing.Point(599, 380);
+            this.work_btn.Name = "work_btn";
+            this.work_btn.Size = new System.Drawing.Size(75, 23);
+            this.work_btn.TabIndex = 8;
+            this.work_btn.Text = "Dolgozz!!";
+            this.work_btn.UseVisualStyleBackColor = true;
+            this.work_btn.Click += new System.EventHandler(this.work_btn_Click);
+            // 
+            // Time_PB
+            // 
+            this.Time_PB.Location = new System.Drawing.Point(526, 351);
+            this.Time_PB.Name = "Time_PB";
+            this.Time_PB.Size = new System.Drawing.Size(233, 23);
+            this.Time_PB.TabIndex = 9;
+            this.Time_PB.Click += new System.EventHandler(this.Time_PB_Click);
+            // 
+            // workh_TB
+            // 
+            this.workh_TB.Location = new System.Drawing.Point(526, 305);
+            this.workh_TB.Maximum = 24;
+            this.workh_TB.Minimum = 1;
+            this.workh_TB.Name = "workh_TB";
+            this.workh_TB.Size = new System.Drawing.Size(233, 45);
+            this.workh_TB.TabIndex = 10;
+            this.workh_TB.Tag = "";
+            this.workh_TB.Value = 1;
+            this.workh_TB.Scroll += new System.EventHandler(this.workh_TB_Scroll);
+            // 
+            // trackbarvalue_lbl
+            // 
+            this.trackbarvalue_lbl.AutoSize = true;
+            this.trackbarvalue_lbl.Location = new System.Drawing.Point(616, 287);
+            this.trackbarvalue_lbl.Name = "trackbarvalue_lbl";
+            this.trackbarvalue_lbl.Size = new System.Drawing.Size(0, 15);
+            this.trackbarvalue_lbl.TabIndex = 11;
+            // 
+            // progressbar_lbl
+            // 
+            this.progressbar_lbl.AutoSize = true;
+            this.progressbar_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.progressbar_lbl.Location = new System.Drawing.Point(622, 351);
+            this.progressbar_lbl.Name = "progressbar_lbl";
+            this.progressbar_lbl.Size = new System.Drawing.Size(0, 15);
+            this.progressbar_lbl.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(638, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "órát fogsz dolgozni.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressbar_lbl);
+            this.Controls.Add(this.trackbarvalue_lbl);
+            this.Controls.Add(this.workh_TB);
+            this.Controls.Add(this.Time_PB);
+            this.Controls.Add(this.work_btn);
+            this.Controls.Add(this.asd_lbl);
+            this.Controls.Add(this.bal_lbl);
+            this.Controls.Add(this.item_lbl);
+            this.Controls.Add(this.menu_lbl);
+            this.Controls.Add(this.penzosszeg_tb);
+            this.Controls.Add(this.visszajaro_lbl);
+            this.Controls.Add(this.ok_btn);
+            this.Controls.Add(this.nev_tb);
+            this.Name = "Form1";
+            this.Text = "Harmadik ora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.workh_TB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nev_tb;
+        private System.Windows.Forms.Button ok_btn;
+        private System.Windows.Forms.Label visszajaro_lbl;
+        private System.Windows.Forms.TextBox penzosszeg_tb;
+        private System.Windows.Forms.Label menu_lbl;
+        private System.Windows.Forms.Label item_lbl;
+        private System.Windows.Forms.Label bal_lbl;
+        private System.Windows.Forms.Label asd_lbl;
+        private System.Windows.Forms.Button work_btn;
+        private System.Windows.Forms.ProgressBar Time_PB;
+        private System.Windows.Forms.TrackBar workh_TB;
+        private System.Windows.Forms.Label trackbarvalue_lbl;
+        private System.Windows.Forms.Label progressbar_lbl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 3.ora/3.ora/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _3.ora
+{
+    public partial class Form1 : Form
+    {
+        int penztarca;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ok_btn_Click(object sender, EventArgs e)
+        {
+            string nev = nev_tb.Text;
+
+            if (nev == "pizza")
+            {   
+                Fuggveny(450);
+            }
+            else if (nev == "hamburger")
+            {                   
+                Fuggveny(750);
+            }
+            else if (nev == "kóla")
+            {                   
+                Fuggveny(650);
+            }
+            else if (nev == "popcorn")
+            {  
+                Fuggveny(100);
+            }
+            else if (nev == "salata")
+            {
+                Fuggveny(1000);
+            }
+            else if (nev == "villanyszamla")
+            {
+                Fuggveny(100000000);
+            }
+            
+                
+                penzosszeg_tb.Text = penztarca.ToString();
+            
+            
+        }
+
+        private void menu_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void work_btn_Click(object sender, EventArgs e)
+        {
+            
+
+            if (penzosszeg_tb.Text != "") 
+            { 
+                penztarca = int.Parse(penzosszeg_tb.Text);
+            
+            }
+
+            int Munkaora = workh_TB.Value;
+            penztarca += Munkaora * 1500;
+            for (int i = 0; i < 100; i++) 
+            {
+                Time_PB.Value = i;
+                progressbar_lbl.Text = i.ToString();
+                Thread.Sleep(10 * Munkaora);
+
+            }
+            penzosszeg_tb.Text = penztarca.ToString();
+            progressbar_lbl.Text = "A munkának vége!";
+            Thread.Sleep(1000);
+            Time_PB.Value = 0;
+            workh_TB.Value = 0;
+        }
+
+        private void workh_TB_Scroll(object sender, EventArgs e)
+        {
+            trackbarvalue_lbl.Text = workh_TB.Value.ToString();
+        }
+
+        private void Time_PB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Fuggveny(int ar)
+        {
+            if (penztarca >= ar)
+            {
+                penztarca -= ar;
+                visszajaro_lbl.Text = penztarca.ToString();
+
+            }
+            else
+            {
+                visszajaro_lbl.Text = "Túl csóró vagy ehez";
+            }
+        }
+    }
+}
